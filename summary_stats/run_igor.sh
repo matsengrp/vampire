607f5e63d4984f49150ea2b73d722e2a50fd68ef
BrandenOlson <branden.olson@colorado.edu>
2018-09-13 10:26:10 -0700

    Print IGoR commands when running run_igor.sh

--- summary_stats/run_igor.sh
+++ summary_stats/run_igor.sh
@@ -22,12 +22,16 @@ rm -rf $WD_PATH/$EVAL_BATCH_NAME*
 
 IGOR_PREFIX="igor -set_wd $WD_PATH"
 
+echo $IGOR_PREFIX -batch $EVAL_BATCH_NAME -read_seqs $INPUT_FILE
 $IGOR_PREFIX -batch $EVAL_BATCH_NAME -read_seqs $INPUT_FILE
 
 IGOR_PREFIX="$IGOR_PREFIX -species $SPECIES -chain $CHAIN"
 
+echo $IGOR_PREFIX -batch $EVAL_BATCH_NAME -align --all
 $IGOR_PREFIX -batch $EVAL_BATCH_NAME -align --all
 
+echo $IGOR_PREFIX -batch $EVAL_BATCH_NAME -evaluate -output --scenarios $NUM_SCENARIOS
 $IGOR_PREFIX -batch $EVAL_BATCH_NAME -evaluate -output --scenarios $NUM_SCENARIOS
 
+echo $IGOR_PREFIX -batch $GEN_BATCH_NAME -generate $NUM_GEN_SEQUENCES --CDR3
 $IGOR_PREFIX -batch $GEN_BATCH_NAME -generate $NUM_GEN_SEQUENCES --CDR3
