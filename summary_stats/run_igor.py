314f91e4eb939766aceac7f747aa05903985230e
BrandenOlson <branden.olson@colorado.edu>
2018-10-01 12:45:47 -0700

    Minor cleanup

--- summary_stats/run_igor.py
+++ summary_stats/run_igor.py
@@ -6,13 +6,12 @@ import numpy as np
 import random
 import re
 
-# May need to move this to /fh/fast/
-sys.path.insert(0, '/home/bolson2/Software/IGoR')
+sys.path.insert(0, '/home/bolson2/Software/IGoR/pygor')
 
-# import pygor
-# from pygor import counters
-# from pygor.counters import bestscenarios
-# from pygor.counters.bestscenarios import bestscenarios
+import pygor
+from pygor import counters
+from pygor.counters import bestscenarios
+from pygor.counters.bestscenarios import bestscenarios
 
 def get_seqs_from_tsv(input_file, \
                       output_file, \
@@ -38,7 +37,6 @@ def run_igor(input_file, \
     df = pd.read_csv(input_file)
     df.columns = ['Sequence']
     generated_seq_count = df.shape[0]
-    
     igor_command = "sh run_igor.sh" + \
             " -w " + igor_wd + \
             " -i " + str(input_file) + \
@@ -48,29 +46,24 @@ def run_igor(input_file, \
             " -b " + gen_batch_name + \
             " -c " + chain + \
             " -s " + species 
-    
     print igor_command
     os.system(igor_command)
-    
     sim_df = pd.read_csv(os.path.join(igor_wd, \
                                       gen_batch_name + "_generated", \
                                       "generated_seqs_werr.csv"),
                          sep=";")
 
 # Get and write annotation dataframe via pygor subpackage routine
-def get_annotations(igor_wd, chain):
+def write_annotations(igor_wd, chain):
     scenarios_filename = "igor_generated/generated_realizations_werr.csv"
     scenarios_file = os.path.join(igor_wd, scenarios_filename)
     model_parms_filename = "igor_evaluate/final_parms.txt"
     model_parms_file = os.path.join(igor_wd, model_parms_filename)
     annotation_dat = bestscenarios.read_bestscenarios_values(scenarios_file, model_parms_file)
-
     annotation_dat['v_gene'] = annotation_dat['v_choice'].apply( \
             lambda(x): extract_gene_from_full_string(x, chain, "V") )
     annotation_dat['j_gene'] = annotation_dat['j_choice'].apply( \
             lambda(x): extract_gene_from_full_string(x, chain, "J") )
-
-    # Write annotations dataset to file
     output_file = "annotations.csv"
     annotation_dat.to_csv(os.path.join(igor_wd, output_file))
     return annotation_dat
@@ -99,8 +92,7 @@ def run_igor_analysis(input_file,
     if not os.path.exists(igor_directory):
         os.makedirs(igor_directory)
     run_igor(sequences_filename, igor_directory)
-    # annotations = get_annotations(igor_directory, chain)
-    return 32
+    write_annotations(igor_directory, chain)
 
 
 if __name__ == "__main__":
