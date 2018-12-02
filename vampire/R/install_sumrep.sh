2649098a46f58913f6d77cdc4881f7dee1ca7f68
Erick Matsen <matsen@fredhutch.org>
2018-12-02 05:06:10 -0800

    new file:   R/install_sumrep.sh

---
+++ vampire/R/install_sumrep.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -eu
+
+conda install -y glpk libgfortran libgfortran-ng libiconv r-base
+for PACKAGE in alakazam ape argparse CollessLike data.table devtools dplyr entropy HDMD jsonlite magrittr pegas Peptides RecordLinkage shazam seqinr stringdist stringr testthat textmineR yaml
+do
+    R --vanilla --slave -e "if(!require($PACKAGE)){install.packages('$PACKAGE', '$CONDA_PREFIX/lib/R/library', repos='https://cloud.r-project.org')}"
+done
+R --vanilla --slave -e "if(!require(Biostrings)){source('https://bioconductor.org/biocLite.R'); biocLite('Biostrings')}"
+
+git submodule update --init --recursive
+# The following is a silly hack to get the sumrep path. If it doesn't work just plug the path in.
+SUMREP_PATH=$(git submodule | grep sumrep | cut -d ' ' -f3)
+R --vanilla --slave -e "library(devtools); devtools::load_all('$SUMREP_PATH')"
