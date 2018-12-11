bba43fa3b6b92b8e3cdbad621414ac6c7e3e112a
Erick Matsen <matsen@fredhutch.org>
2018-12-11 09:10:37 -0800

    -

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
-for tag in latest-gpu-py3 latest-gpu-py3
+for tag in latest-py3 latest-gpu-py3
 do
     docker build -t matsen/tcr-vae:$tag --build-arg tftag=$tag .
     docker push matsen/tcr-vae:$tag
