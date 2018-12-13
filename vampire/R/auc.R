c419e6b44c55e1e5717e6900fe90cad9f3e3b592
Erick Matsen <matsen@fredhutch.org>
2018-12-12 18:22:17 -0800

    -

--- vampire/R/auc.R
+++ vampire/R/auc.R
@@ -7,15 +7,13 @@ suppressMessages(library(pROC))
 parser = ArgumentParser(description="Calculate AUC for Pgen or Pvae in a cross-fooling comparison.")
 
 parser$add_argument('--pvae', action='store_true', help="We are processing Pvae, rather than default Pgen.")
-parser$add_argument('--plot', help="Optional destination path for a PDF plot.")
+parser$add_argument('--plot', help="Optional destination path for a PDF ROC plot.")
 parser$add_argument('in_real', help="A CSV with a column of per-sequence probabilities of real sequences.")
 parser$add_argument('in_generated', help="A CSV with a column of per-sequence probabilities of synthetically generated sequences.")
 parser$add_argument('out_csv', help="Desired location for single-row output CSV. Will over-write with abandon.")
 
 args = parser$parse_args()
 
-# Note that taking exp shouldn't make a difference for AUC, but I just wanted
-# to be sure that we were making analyses as similar as possible.
 if(args$pvae) {
     header=TRUE
     idx=1
@@ -28,6 +26,10 @@ if(args$pvae) {
     col_name = 'auc_pgen'
 }
 
+
+# Note that taking exp shouldn't make a difference for AUC because it's
+# monotonic, but I just wanted to be sure that we were making analyses between
+# the cases identical.
 
 read_and_real = function(path, real) {
     P = read.csv(path, header=header)[[idx]]
