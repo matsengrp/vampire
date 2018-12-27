e4183a63df413455e35a9deae46c084329002d88
Erick Matsen <matsen@fredhutch.org>
2018-12-27 04:56:05 -0800

    Restarter actually works on mini

--- vampire/restarter.py
+++ vampire/restarter.py
@@ -1,8 +1,9 @@
-import re
+import sys
 import time
 
 import click
 import delegator
+import pexpect
 
 
 @click.command()
@@ -12,39 +13,38 @@ def cli(command):
     This includes doing filters as well as deduplicating on vjcdr3s.
     """
 
-    retry=0
-    max_n_retries = 100
-    sleep_time=10
+    max_n_retries = 10
+    timeout = 300
 
+    retry = 0
     click.echo(f"Running `{command}`.")
 
     while True:
 
         try:
-            main_command = delegator.run(command)
-            main_command.expect("scons: done building targets")
+            main_command = pexpect.spawn(command, logfile=sys.stdout.buffer)
+            main_command.expect("scons: done building targets", timeout=timeout)
             click.echo("SCons has completed.")
             break
-        except TIMEOUT:
+        except pexpect.TIMEOUT:
             pass
+        except pexpect.EOF:
+            click.echo("Process stopped without SCons completing.")
 
-        # Once we've timed out, check to see if it seems like things are still active.
-
-        jobs_remaining = 1
-
-        while jobs_remaining > 0:
+        while True:
             c = delegator.run('squeue -u matsen -M beagle | tail -n +3 | wc -l')
             jobs_remaining = int(c.out)
+            if jobs_remaining == 0:
+                break
             click.echo("{jobs_remaining} jobs still running.")
-            time.sleep(sleep_time)
-            continue
+            time.sleep(60)
 
-        click.echo("Things seem stuck. Last output:")
-        click.echo(main_command.out.rstrip().split('\n')[-1])
+        if main_command.exitstatus != 0:
+            sys.exit(main_command.exitstatus)
 
         if retry < max_n_retries:
-            retry = retry+1
-            click.echo(f"Restarting for retry {retry} of {max_n_retries}...")
+            retry = retry + 1
+            click.echo(f"Things seem stuck. Restarting for retry {retry} of {max_n_retries}...")
 
 
 if __name__ == '__main__':
