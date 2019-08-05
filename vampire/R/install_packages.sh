56ef8084bc181152647fba767b57f3f1d5d77b88
Erick Matsen <ematsen@gmail.com>
2019-08-05 16:09:10 -0700

    dropping pegas

--- vampire/R/install_packages.sh
+++ vampire/R/install_packages.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 set -eu
 
-conda install -y glpk libgfortran libgfortran-ng libiconv r-base r-git2r
+conda install -y -c anaconda glpk libgfortran libgfortran-ng libiconv r-base r-git2r
 # I had to add the following line to get svglite to install. You may not need it.
 conda install -y -c conda-forge r-gdtools
-conda install -y -c bioconda r-pegas
 for PACKAGE in alakazam ape argparse cowplot CollessLike data.table devtools dplyr entropy HDMD hexbin jsonlite latex2exp magrittr Peptides pROC RecordLinkage shazam seqinr stringdist stringr svglite testthat textmineR yaml
 do
     R --vanilla --slave -e "if(!require($PACKAGE)){install.packages('$PACKAGE', '$CONDA_PREFIX/lib/R/library', repos='https://cloud.r-project.org')}"
