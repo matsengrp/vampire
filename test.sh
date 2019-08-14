667474c44d7eaf44a917f55a7bd58465ca021fef
Erick Matsen <ematsen@gmail.com>
2019-08-14 15:12:13 -0700

    run demo.sh in test script

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ cd vampire
 git submodule update --init --recursive
 SUMREP_PATH=$(git submodule | grep sumrep | cut -d ' ' -f3); /opt/conda/bin/conda run -n vampire R --vanilla --slave -e "library(devtools); devtools::load_all('$SUMREP_PATH')"
 /opt/conda/bin/conda run -n vampire pip install .
-cd vampire/
-/opt/conda/bin/conda run -n vampire scons
+cd vampire/demo
+/opt/conda/bin/conda run -n vampire demo.sh
